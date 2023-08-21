1645f5efe3da46b184a1f2b891b239be25a63189
serhiiderkach <serhiiderkach1995@gmail.com>
2023-08-21 11:10:50 -0500

    new commit

--- src/components/TaskCard/components/DueDate/components/DateForm/DateForm.js
+++ src/components/TaskCard/components/DueDate/components/DateForm/DateForm.js
@@ -1,3 +1,23 @@
-const DateForm () => {
-    
-}
+import Calendar from './calendar.svg';
+
+const DateForm = ({ value, onChange }) => (
+    <div className={classes.dateForm}>
+        <label htmlFor='date' className={classes.date__label}>Select Due Date</label>
+        <form className={classes.date__form}>
+            <input
+                placeholder='dd.mm.yyyy'
+                id='date' 
+                name='date' 
+                type='date'
+                onChange={onChange}
+                value={value} 
+                className={classes.date__input}                
+            />
+            <img src={Calendar} alt='calendar'/>   
+        </form>
+    </div>
+);
+ 
+export { DateForm };
+
+
